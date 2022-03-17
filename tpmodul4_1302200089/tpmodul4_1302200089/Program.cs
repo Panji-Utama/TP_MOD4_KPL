91a271720d60daecff10fa88ae0aaf895356f261
FIRMAN PANJI <fpu2468@gmail.com>
2022-03-17 22:55:55 +0700

    menambahkan class DataGeneric

--- tpmodul4_1302200089/tpmodul4_1302200089/Program.cs
+++ tpmodul4_1302200089/tpmodul4_1302200089/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302200089
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("NIM anda : ");
+            String nim = Console.ReadLine();
+
+            DataGeneric<string> dataGeneric = new DataGeneric<string>(nim);
+            dataGeneric.PrintData();
+
+        }
+    }
+
+    class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T Data)
+        {
+            this.data = Data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("NIM yang diinput adalah " + data);
+        }
+    }
+}
